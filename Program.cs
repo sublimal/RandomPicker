afb557b4bcf3b0ab433e2fb3e7a36509868fb06f
sublimal <sublimal@github>
2019-06-19 04:01:31 -0700

    Added delete option. Remove Shell32 ref

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿using Shell32;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,26 +12,28 @@ namespace RandomPicker
         static Random random = new Random();
         enum UserChoice
         {
-            Cancel = 0,
+            Quit = 0,
             Yes = 1,
             Next = 2,
             Previous = 3,
-            Details = 4,
-            Find = 5
+            Find = 4,
+            Delete = 5
         }
 
         [STAThread]
         static void Main(string[] args)
         {
+            Console.WriteLine("Scanning...");
             var files = getAllMediaFiles(Directory.GetCurrentDirectory());
+            Console.WriteLine("Done.");
 
-            UserChoice choice;
-            String fileName;
-
+            
             // Stop if no files
             if (!files.Any())
+            {
+                Console.WriteLine("No files found.");
                 return;
-
+            }
             // Randomize list (Fisher-Yates)
             int n = files.Count;
             while (n > 1)
@@ -44,7 +45,10 @@ namespace RandomPicker
                 files[n] = value;
             }
 
+            UserChoice choice;
+            String fileName;
             int picker = 0;
+     
             do
             {
                 // Go through the randomized list
@@ -72,17 +76,28 @@ namespace RandomPicker
                         else
                             picker--;
                         break;
-                    case UserChoice.Details:
-                        printDetails(fileName);
-                        break;
-                    case UserChoice.Cancel:
+                    case UserChoice.Quit:
                         return;
+                    case UserChoice.Delete:
+                        if (fileName == "") break;
+                        Console.WriteLine("\nConfirm [y/N]:");
+                        var confirm = Console.ReadKey();
+                        if (confirm.KeyChar == 'y')
+                        {
+                            File.Delete(fileName);
+                            files[picker] = "";
+                            if (picker == files.Count - 1)
+                                continue; // Last one
+                            else
+                                picker++;
+                        }
+                        break;
                     case UserChoice.Yes:
+                        if (fileName == "") break;
                         System.Diagnostics.Process.Start(fileName);
                         break;
                     case UserChoice.Find:
-                        Console.WriteLine();
-                        Console.WriteLine("Input search string:");
+                        Console.WriteLine("\nInput search string:");
                         var search = Console.ReadLine();
                         var found = files.FirstOrDefault(f => f.ToLower().Contains(search.ToLower()));
                         if (found == null)
@@ -92,21 +107,25 @@ namespace RandomPicker
                         break;
                 }
 
-            } while ((int)choice > 1);
+            } while (choice > UserChoice.Quit);
         }
 
         private static String generatePromptString(String fileName, bool next, bool previous)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append(String.Format("\nPlay \"{0}\"? [Y]es, ", fileName.Split('\\').Last()));
-
+            if (fileName == "") {
+                sb.Append("\n(deleted)");
+            } else {
+                sb.Append(String.Format("\nPlay \"{0}\"? [Y]es,", fileName));
+                sb.Append(" [D]elete,");
+            }
             if (next)
                 sb.Append(" [N]ext,");
 
             if (previous)
                 sb.Append(" [P]revious,");
 
-            sb.Append(" [C]ancel, [D]etails, [F]ind.");
+            sb.Append(" [F]ind, [Q]uit.");
 
             return sb.ToString();
         }
@@ -119,12 +138,12 @@ namespace RandomPicker
             {
                 case 'y':
                     return UserChoice.Yes;
-                case 'c':
-                    return UserChoice.Cancel;
+                case 'd':
+                    return UserChoice.Delete;
                 case 'p':
                     return UserChoice.Previous;
-                case 'd':
-                    return UserChoice.Details;
+                case 'q':
+                    return UserChoice.Quit;
                 case 'f':
                     return UserChoice.Find;
                 default:
@@ -169,47 +188,6 @@ namespace RandomPicker
 
             // Return all files collected
             return files;
-        }
-
-        private static void printDetails(String filePath)
-        {
-            List<string> arrHeaders = new List<string>();
-            var folderPath = Path.GetDirectoryName(filePath);
-
-            Shell shell = new Shell();
-            Folder shellFolder = shell.NameSpace(folderPath);
-            FolderItem shellFile = shellFolder.ParseName(Path.GetFileName(filePath));
-            // Video length id 27
-
-            // Get all possible headers
-            for (int i = 0; i < short.MaxValue; i++)
-            {
-                string header = shellFolder.GetDetailsOf(null, i);
-                if (String.IsNullOrEmpty(header))
-                    break;
-                arrHeaders.Add(header);
-            }
-
-            // To look more pretty
-            Console.WriteLine();
-            Console.WriteLine("Directory: \t " + folderPath);
-
-            // Print only values available
-            for (int i = 0; i < arrHeaders.Count; i++)
-            {
-                var header = arrHeaders[i];
-                var value = shellFolder.GetDetailsOf(shellFile, i).Trim();
-                if (!String.IsNullOrWhiteSpace(value))
-                {
-                    System.Diagnostics.Debug.WriteLine($"{i}\t{header}: {value}");
-
-                    if (header.Length > 7)
-                        Console.WriteLine($"{header}:\t {value}");
-                    else
-                        Console.WriteLine($"{header}:\t\t {value}");
-
-                }
-            }
         }
     }
 }
